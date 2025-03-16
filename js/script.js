394b6e6a5e0e089ac7bdfb6ad90975c90b5c3311
Benjamin Risbey <benrisbey2000@gmail.com>
2025-03-16 20:08:01 +0000

    Add website files and track large images with Git LFS

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const links = document.querySelectorAll('.mega-submenu a');
+
+// General hover function for changing images and text
+function handleHover(linkSelector, imageId, textId, defaultImage, defaultText) {
+  const imageDiv = document.getElementById(imageId);
+  const textDiv = document.getElementById(textId);
+
+  linkSelector.forEach(function(link) {
+    link.addEventListener('mouseenter', function() {
+      const newImage = link.getAttribute('data-image');
+      const newText = link.getAttribute('data-text');
+      imageDiv.src = newImage;
+      textDiv.textContent = newText;
+    });
+
+    link.addEventListener('mouseleave', function() {
+      imageDiv.src = defaultImage;
+      textDiv.textContent = defaultText;
+    });
+  });
+}
+
+// Apply hover logic for different sections
+handleHover(links, 'image-1', 'image-text-1', 'images/navigationImages/men-default.jpg', 'MEN');
+handleHover(links, 'image-2', 'image-text-2', 'images/navigationImages/women-default.jpg', 'WOMEN');
+handleHover(links, 'image-3', 'image-text-3', 'images/navigationImages/explore-default.jpg', 'EXPLORE');
+
+
+
+// General toggle function for dropdowns
+function toggleDropdown(linkSelector, dropdownSelector) {
+  const link = document.querySelector(linkSelector);
+  const dropdown = document.querySelector(dropdownSelector);
+
+  link.addEventListener('click', function() {
+    dropdown.classList.toggle('active');
+  });
+}
+
+// Mobile Menu
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+  document.querySelector('.mobile-main-menu').classList.toggle('active');
+});
+
+// Men, Women, and Explore dropdowns
+toggleDropdown('.men-link', '.men-mega-dropdown');
+toggleDropdown('.women-link', '.women-mega-dropdown');
+toggleDropdown('.explore-link', '.explore-mega-dropdown');
+
+
---
+++ js/women.js
@@ -0,0 +1,55 @@
+import { products } from './womenproducts.js';
+
+const productContainer = document.getElementById("product-container");
+let selectedCategory = "";
+
+// Function to display products
+function displayProducts(filteredProducts) {
+  productContainer.innerHTML = filteredProducts.map(product => `
+    <div class="product-card">
+      ${product.sale ? '<div class="badge">SALE</div>' : ''}
+      ${product.new ? '<div class="badge">NEW</div>' : ''}
+      ${product.bestSeller ? '<div class="badge">BEST SELLER</div>' : ''}
+      ${product.membersExclusive ? '<div class="badge">MEMBERS DISCOUNT</div>' : ''}
+      <div class="product-tumb">
+        <img src="${product.image}" alt="" onmouseover="this.src='${product.imagehover}';" onmouseout="this.src='${product.image}';">
+      </div>
+      <div class="product-details">
+        <span class="product-catagory">${product.category.join(", ")}</span>
+        <h4><a href="">${product.name}</a></h4>
+        <p>${product.bio}</p>
+        <div class="product-bottom-details">
+          <div class="product-price">
+            ${product.priceOld !== undefined 
+              ? `<small>£${product.priceOld.toFixed(2)}</small>`
+              : ''} 
+            £${product.price.toFixed(2)}
+          </div>
+          <div class="product-links">
+            <a href=""><i class="fa fa-heart"></i></a>
+            <a href=""><i class="fa fa-shopping-cart"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `).join("");
+}
+
+// Function to filter products based link clicked
+function filterProducts() {
+  // Get URL parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  selectedCategory = urlParams.get("category");
+
+  // Filter products based on the selected category
+  let filtered = products.filter(function(product) {
+    return selectedCategory === "" || product.category.includes(selectedCategory);
+  });
+
+  // Display the filtered products
+  displayProducts(filtered);
+}
+
+// Initial Display
+filterProducts();
+
